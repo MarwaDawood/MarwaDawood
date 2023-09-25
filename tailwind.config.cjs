6c7a5e5c8f682bdd4da64b7dfaf0eef5fcbd2882
MarwaDawood <marwa.dawood@hotmail.com>
2023-09-25 04:16:52 +0100

    third commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'mainblue': '#4C77AB',
+			},
+			fontFamily: {
+			display: ['Imprima'],
+		},
 	},
 	plugins: [],
+}
 }
