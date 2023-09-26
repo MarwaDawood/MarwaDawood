c25a69cb6c0251e58062cd138158520f8ee32cc2
MarwaDawood <marwa.dawood@hotmail.com>
2023-09-27 00:43:44 +0100

    fifth commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,15 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react(), tailwind()]
+export default defineConfig
+({
+  integrations: [react(), tailwind()],
+  routes: [
+  { path: '/', component: '@layout/MainLayout.astro' },
+  { path: '/#', component: './src/components/home/Home.astro' },
+  { path: '/about', component: './src/components/about/About.astro' },
+]
 });
+
+
+
