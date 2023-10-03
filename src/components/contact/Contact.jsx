cf1866ed53d60210b650ca3c72cabb1620439d99
MarwaDawood <marwa.dawood@hotmail.com>
2023-10-03 14:58:28 +0100

    feedback four

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,14 +29,14 @@ const Contact = () => {
       <div className="lg:mt-10 mt-4 pb-10 md:m-10 m-5">
       <form ref={form} onSubmit={sendEmail}> 
 
-      <input type='text' name='name' placeholder='Your Full Name' required  className="contactme border-solid border-[4px] border-maingreen w-full lg:p-6 p-3 rounded-xl bg-white resize-none lg:my-5 my-3 transition-transform transform hover:scale-105"/> 
+      <input type='text' name='name' placeholder='Your Full Name' required  className="contactme border-solid border-[3px] border-maingreen w-full lg:p-6 p-3 rounded-xl bg-white resize-none lg:my-5 my-3 transition-transform transform hover:scale-105"/> 
 
-      <input type='email' name='email' placeholder='Your Email' required className="contactme w-full lg:p-6 p-3 rounded-xl resize-none lg:my-5 my-3 transition-transform transform hover:scale-105"/>
+      <input type='email' name='email' placeholder='Your Email' required className="contactme border-solid border-[5px] border-maingreen w-full lg:p-6 p-3 rounded-xl resize-none lg:my-5 my-3 transition-transform transform hover:scale-105"/>
 
-      <textarea name='message' placeholder='Your Message' rows='7' required className="contactme w-full lg:p-6 p-3 rounded-xl bg-white resize-y lg:my-5 my-3 transition-transform transform hover:scale-105 lg:mb-10"></textarea>
+      <textarea name='message' placeholder='Your Message' rows='7' required className="contactme w-full lg:p-6 p-3 rounded-xl bg-white resize-y lg:my-5 my-3 transition-transform transform hover:scale-105 lg:mb-10 border-solid border-[7px] border-maingreen"></textarea>
 
 
-      <button type='submit' className="sendme shadow-md shadow-black inline-block lg:px-10 px-5 py-2 text-lg lg:text-2xl font-medium text-display cursor-pointer text-black bg-white border-2 border-[#82A047] rounded-md transition-transform transform hover:scale-105 ml-[28%] md:ml-[38%]"> Send Message </button>
+      <button type='submit' className="sendme shadow-md shadow-black inline-block lg:px-10 px-3 py-1 text-lg lg:text-2xl font-medium text-display cursor-pointer text-black bg-white border-solid border-[2px] border-maingreen rounded-md transition-transform transform hover:scale-105 ml-[31%] md:ml-[39%]"> Send Message </button>
       </form>
       </div>
       </section>
